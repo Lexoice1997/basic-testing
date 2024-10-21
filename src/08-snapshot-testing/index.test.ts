cf3b83278dda0f11a77bd656e0f791705ad60b29
Azamat <az.berdimuratov@uzinfocom.uz>
2024-10-21 22:50:41 +0500

    feat: add snapshot test

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,35 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
+  interface User {
+    id: number;
+    name: string;
+  }
+
+  const firstClass: User[] = [
+    { id: 1, name: 'Azamat' },
+    { id: 2, name: 'Dawlet' },
+  ];
+  const secondClass: User[] = [
+    { id: 3, name: 'Begis' },
+    { id: 4, name: 'Islam' },
+  ];
+
+  const expectedLinkedList = {
+    value: { id: 1, name: 'Azamat' },
+    next: {
+      value: { id: 2, name: 'Dawlet' },
+      next: {
+        value: null,
+        next: null,
+      },
+    },
+  };
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList(firstClass)).toStrictEqual(expectedLinkedList);
   });
 
-  // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList(secondClass)).toMatchSnapshot();
   });
 });
